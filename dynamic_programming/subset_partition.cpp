5e5052c60703887900171675cd74bdb025f01372
Weikang Chen <weikangchen1984@gmail.com>
2016-05-28 11:20:23 -0400

    add subset_partition.cpp and tree/graph

---
+++ dynamic_programming/subset_partition.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool partition(vector<int> arr) {
+    if (arr.empty()) return false;
+    const int n = arr.size();
+
+    int sum = 0;
+    for(int i = 0; i < n; ++i)
+        sum += arr[i];
+    //cout << sum << endl;
+    if (sum & 1) return false;
+    int tar = sum / 2;
+    //cout << tar << endl;
+
+    vector<vector<bool> > dp(n, vector<bool>(tar+1, false));
+    dp[0][0] = dp[0][arr[0]] = true;
+    for (int i = 1; i < n; ++i) {
+        dp[i][0] = true;
+        for (int j = 0; j <= tar; ++j) {
+            dp[i][j] = dp[i-1][j];
+            if (j - arr[i] >= 0)
+                dp[i][j] = dp[i][j] || dp[i-1][j-arr[i]];
+        }
+    }
+
+    return dp[n-1][tar];
+}
+
+int main(int argc, char *argv[])
+{
+    int a[] = {1,2,3,4,5,6,7};
+    vector<int> arr(a, a+sizeof(a)/sizeof(int));
+    bool p = partition(arr);
+    cout << (p ? "True" : "False") << endl;
+    return 0;
+}
